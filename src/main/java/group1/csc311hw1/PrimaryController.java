8df0f215738656381fa081c1fd7d86d8446a7728
nka2001 <nandrle01@gmail.com>
2022-02-19 11:36:43 -0500

    Homework 1, Load from Database done

--- src/main/java/group1/csc311hw1/PrimaryController.java
+++ src/main/java/group1/csc311hw1/PrimaryController.java
@@ -1,6 +1,12 @@
 package group1.csc311hw1;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -21,11 +27,48 @@ public class PrimaryController {
     @FXML
     private void loadFromDB(ActionEvent event) {
         
+        ObservableList<String> i = listView.getItems();
+        i.clear();
+        textField.setText("0");
+         
+        
+        textField.setText(Integer.toString(i.size()));
+        
+        retrieveData(i);
+        
+        textField.setText(Integer.toString(i.size()));
         
         
         
         
+    }
+    public void retrieveData(ObservableList<String> i){
+        String dbURL = "";
+        Connection c = null;
         
+        try{//connect to the database block
+            dbURL = "jdbc:ucanaccess://.//Games.accdb";
+            c = DriverManager.getConnection(dbURL);
+            System.out.println("Connected");
+        }catch(SQLException e){
+            System.out.println("Database not found");
+        }
+        try{
+            String tablename = "VideoGames";
+            Statement st = c.createStatement();
+            ResultSet r = st.executeQuery("Select * from " + tablename);
+            
+            
+            while(r.next()){
+                int id = r.getInt("ID");
+                String title = r.getString("Title");
+                double price = r.getDouble("Price");
+                String esrbRating = r.getString("ESRB Rating");
+                i.add(id+ " " + title+ " " + price+ " " +esrbRating);
+            }
+        }catch(SQLException e){
+            System.out.println("error");
+        }
         
         
     }
