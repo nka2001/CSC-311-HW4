efcede20736812ffd911f66d5353a8a3b7b7c56c
nka2001 <nandrle01@gmail.com>
2022-02-23 19:47:06 -0500

    Homework 1, Final Version

--- src/main/java/group1/csc311hw1/PrimaryController.java
+++ src/main/java/group1/csc311hw1/PrimaryController.java
@@ -20,6 +20,10 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 
+/**
+ * GUI class, contains the methods to load data from a database, and load data from a JSON file into a database, and to display the data from the database 
+ * @author nicka
+ */
 public class PrimaryController {
 
     @FXML
@@ -35,7 +39,13 @@ public class PrimaryController {
     
     private ObservableList<String> i; //obervable list for listview
 
+   
     @FXML
+     /**
+     * loads data from the database when the button is clicked, retrieveData is called to retrieve the data from the database 
+     * @param event
+     * @throws SQLException 
+     */
     private void loadFromDB(ActionEvent event) throws SQLException {//this will load the listview with data from the access database 
         i = listView.getItems();
         i.clear();//this will clear the listview 
@@ -51,6 +61,11 @@ public class PrimaryController {
          
 
     }
+    /**
+     * retrieves data from the database and displays the contents in the listview
+     * @param i (observableList of type string)
+     * @throws SQLException 
+     */
 
     public void retrieveData(ObservableList<String> i) throws SQLException {
 
@@ -83,6 +98,13 @@ public class PrimaryController {
         }
     }
 
+    
+    /**
+     * LoadFromJSON will load data into the database from a JSON file 
+     * @param event
+     * @throws SQLException
+     * @throws FileNotFoundException 
+     */
     @FXML
     private void loadFromJSON(ActionEvent event) throws SQLException, FileNotFoundException {
         
@@ -106,7 +128,7 @@ public class PrimaryController {
             b.setPrettyPrinting();
             Gson gson = b.create();
 
-            FileReader fr = new FileReader("C:\\Users\\soblab\\Documents\\NetBeansProjects\\CSC-311-HW-1\\src\\main\\java\\group1\\csc311hw1\\games.json");
+            FileReader fr = new FileReader("games.json");
             VideoGame[] vg = gson.fromJson(fr, VideoGame[].class); // reads from the JSON file and put the output into an array of videogame objects
 
             for (int k = 0; k < vg.length; k++) {
@@ -135,7 +157,10 @@ public class PrimaryController {
             c.close();
         }
     }
-
+    /**
+     * closes the program upon clicking the menu item close 
+     * @param event 
+     */
     @FXML
     private void CloseHW(ActionEvent event
     ) {//close method, upon clicking from the menu drop down "Close" it will cause the program to stop runnning
@@ -144,7 +169,10 @@ public class PrimaryController {
         System.exit(0);
 
     }
-
+/**
+ * deletes the entire database, this is needed so no duplicates are added to the database, the listview is not updtaed when this happens
+ * @throws SQLException 
+ */
     public void deleteList() throws SQLException {
         
         
--- src/main/java/group1/csc311hw1/VideoGame.java
+++ src/main/java/group1/csc311hw1/VideoGame.java
@@ -7,34 +7,72 @@ public class VideoGame {
     private double price;
     private String esrb;
     
+    
+    /**
+     * default constructor, creates default values for a video game 
+     */
     public VideoGame(){//default constructor
         this.title = "default";//sets all three member variables to default values
         this.price = 0.00;
         this.esrb = "n/a";
     }
+    /**
+     * overloaded constructor, accepts two strings and a double for title, price, and ESRB rate
+     * @param t
+     * @param p
+     * @param r 
+     */
     public VideoGame(String t, double p, String r){//overloaded constructor 
         this.title = t;
         this.price = p;
         this.esrb = r;
     }
+    /**
+     * sets the title of a video game
+     * @param t 
+     */
     public void setTitle(String t){//setters, they set the values 
         this.title = t;
     }
+    /**
+     * sets the price of a video game
+     * @param p 
+     */
     public void setPrice(double p){
         this.price = p;
     }
+    /**
+     * sets the esrb rating of a videogame
+     * @param r 
+     */
     public void setEsrbRating(String r){
         this.esrb = r;
     }
+    /**
+     * returns the title of a videogame
+     * @return 
+     */
     public String getTitle(){//getters, they return values
         return this.title;
     }
+    /**
+     * returns the price of a videogame
+     * @return 
+     */
     public double getPrice(){
         return this.price;
     }
+    /**
+     * returns the rating of a videogame 
+     * @return 
+     */
     public String getRating(){
         return this.esrb;
     }
+    /**
+     * toString method to return the title, price, and esrb rating for the listview
+     * @return 
+     */
     public String toString(){
         String str = title + ", "+ price + ", " + esrb;
         return str;
